cd9250aa1b2627b9687beded87efbd3ada51893b
ihyun <okyee8335@naver.com>
2023-02-18 23:22:49 +0900

    1 Week Preparation Kit - Day5

--- HackerRank/Reursive Digit Sum.ts
+++ HackerRank/Reursive Digit Sum.ts
@@ -1,14 +1,18 @@
-
-function superDigit(n, k) {
-    function splitSum(num) {
+function superDigit(n: any, k: any) {
+    function splitSum(num: any) {
         const numArr = String(num).split("").map(Number);
         // ↳const numArr = String(num).split("").map(e => Number(e));
         return numArr.reduce((acc, curr) => acc + curr, 0)
     }
 
-    let p = n.repeat(k);
-    while (Number(p) >= 10) {
-        p = splitSum(p)
+    let n1 = splitSum(n);
+    // @ts-ignore
+    let n2 = String(n1).repeat(k);
+
+    while (Number(n2) >= 10) {
+        n2 = splitSum(n2)
     }
-    return p
+    return n2
 }
+
+console.log(superDigit(148, 3))
