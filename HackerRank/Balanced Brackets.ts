032c15ac8906338d866dce35b62e9537000fd41c
ihyun <okyee8335@naver.com>
2023-02-20 13:33:29 +0900

    1 Week Preparation Kit - Day6

--- HackerRank/Balanced Brackets.ts
+++ HackerRank/Balanced Brackets.ts
@@ -1,23 +1,18 @@
 function isBalanced(s) {
-    if(s.length % 2 !== 0) return 'NO';
+    if (s.length % 2 !== 0) return 'NO';
 
     let string = s;
 
-    while(string.length >= 1){
-        if(string.includes('()')){
+    while (string.length >= 1) {
+        if (string.includes('()')) {
             string = string.replace('()', '');
-        } else if(string.includes('[]')){
+        } else if (string.includes('[]')) {
             string = string.replace('[]', '');
-        }else if(string.includes('{}')){
+        } else if (string.includes('{}')) {
             string = string.replace('{}', '');
-        }else {
+        } else {
             break;
         }
     }
-
-    if(string.length >= 1){
-        return 'NO'
-    }
-
-    return 'YES'
+    return s.length > 0 ? 'NO' : 'YES'
 }
