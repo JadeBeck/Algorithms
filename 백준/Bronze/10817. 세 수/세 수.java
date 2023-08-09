28e1ee9c465dbea67e23cc5d628149263a0235b8
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-08-09 19:29:58 +0900

    [Bronze III] Title: 세 수, Time: 208 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int[] array = {a, b, c};
+        Arrays.sort(array);
+        
+        System.out.println(array[1]);
+    }
+}
