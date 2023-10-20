75c7c3f2aa9c2846eca042290e4833b3a24dd32d
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-10-21 00:51:47 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,32 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[3];
+		
+		//무한루프
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[0] = Integer.parseInt(st.nextToken());
+			arr[1] = Integer.parseInt(st.nextToken());
+			arr[2] = Integer.parseInt(st.nextToken());
+			
+			//0 0 0 입력시 무한루프 종료
+			if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+			
+			//정렬
+			Arrays.sort(arr);
+			
+			if(arr[2] >= arr[0] + arr[1]) {
+				System.out.println("Invalid");
+			}else if(arr[0] == arr[1] && arr[1] == arr[2]) {
+				System.out.println("Equilateral");
+			}else if(arr[0] == arr[1] || arr[1] == arr[2] || arr[0] == arr[2]) {
+				System.out.println("Isosceles");
+			}else {
+				System.out.println("Scalene");
+			}
+		}
+	}
 
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-
-boolean b = true;
-
-while (b) {
-
-int x = sc.nextInt();
-int y = sc.nextInt();
-int z = sc.nextInt();
-
-if (x == 0 && y == 0 && z == 0) {
-break;
-}
-
-if (x >= y + z || y >= x + z || z >= x + y) {
-System.out.println("Invalid");
-}else if (x == y && x == z && y == z) {
-System.out.println("Equilateral");
-} else if (x == y || x == z || y == z) {
-System.out.println("Isosceles");
-} else if (x != y && x != z && y != z) {
-System.out.println("Scalene");
-}
-
-}
-}
 }
