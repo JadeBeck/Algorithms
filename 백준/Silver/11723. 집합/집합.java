1b6b9b2cb67579184dcc620b8c54cea28ff6d064
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-10-25 09:44:50 +0900

    [Silver V] Title: 집합, Time: 1112 ms, Memory: 312716 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -7,41 +7,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = null;
 
-        int n = Integer.parseInt(br.readLine());
         int S = 0;
-        while(n-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            int num = 0;
+        int M = Integer.parseInt(br.readLine());
 
-            switch(str) {
-                case "add" :
-                    num = Integer.parseInt(st.nextToken()) - 1;
-                    S = S | (1 << num);
-                    break;
-                case "remove" :
-                    num = Integer.parseInt(st.nextToken()) - 1;
-                    S = S & ~(1 << num);
-                    break;
-                case "check" :
-                    num = Integer.parseInt(st.nextToken()) - 1;
-                    int temp = S & (1 << num);
-                    sb.append(((temp == 0) ? 0 : 1) + "\n");
-                    break;
-                case "toggle" :
-                    num = Integer.parseInt(st.nextToken()) - 1 ;
-                    S = S ^ (1 << num);
-                    break;
-                case "all" :
-                    S = (1 << 21) - 1;
-                    break;
-                case "empty" :
-                    S = 0;
-                    break;
+        while (M-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            
+            if(str.equals("all")) S = (1 << 21) - 1;
+            else if(str.equals("empty")) S = 0;
+            else {
+                int num = Integer.parseInt(st.nextToken());
+                if(str.equals("add"))
+                    S |= (1 << num);
+                else if(str.equals("remove"))
+                    S &= ~(1 << num);
+                else if(str.equals("check"))
+                    sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
+                else if(str.equals("toggle"))
+                    S ^= (1 << num);
             }
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
