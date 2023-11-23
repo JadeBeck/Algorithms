d6c9a4d90d0539ccdcfb10fa75101573af724b7a
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-11-24 08:30:07 +0900

    [Bronze III] Title: 약수 구하기, Time: 132 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -11,11 +11,9 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
 
         List<Integer> divisor = new ArrayList<>();
-        for (int i = 1; i <= N; i++) {
-            if (N % i == 0) divisor.add(i);
-        }
+        for (int i = 1; i <= N; i++) if (N % i == 0) divisor.add(i);
 
-        if (divisor.size() > K-1) System.out.println(divisor.get(K-1));
+        if (divisor.size() >= K) System.out.println(divisor.get(K-1));
         else System.out.println(0);
     }
 }
