b005f04cd590707f2665739b6d307a4b2d8c940a
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-11-30 19:49:49 +0900

    [Silver IV] Title: 스택 2, Time: 1252 ms, Memory: 239232 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -2,17 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int n = Integer.parseInt(bf.readLine());
+        int n = Integer.parseInt(br.readLine());
 
         //Stack? LIFO, DFS에 이용됨
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bf.readLine());
+            st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
 
             if (num == 1) {
