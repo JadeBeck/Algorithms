e7f55730e80f1a1aed0adfd1b35cb2c6142a4575
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-12-06 10:32:23 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 452 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -3,31 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int count1 = 1;
-	static int count2 = 0;
-    
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		fib(N);
-		fibonacci(N);
-		System.out.println(count1 + " " + count2 );	
-	}
-	
-	public static int fib(int n) {		
-		if(n <= 2) return 1;
-		count1++;
-		return fib(n - 1) + fib(n - 2);
-	}
-	
-	public static int fibonacci(int n) {
-		int[] f = new int[n+1];
-		f[1] = 1;
-		f[2] = 1;
-		for(int i = 3; i <= n; i++) {
-			count2++;
-			f[i] = f[i-1] + f[i-2];
-		}
-		return f[n];
-	}					
+    static int count1, count2 = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        fib(N);
+        fibonacci(N);
+        System.out.println(count1 + " " + count2);
+    }
+
+    public static int fib(int n) {
+        if (n <= 2) {
+            count1++;
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static void fibonacci(int n) {
+        int[] fibonacci = new int[n + 1];
+        fibonacci[1] = 1;
+        fibonacci[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            count2++;
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+    }
 }
