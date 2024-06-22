6189456a66a86151edbdb942b2d04aa8408cdc58
Ihyun <97588819+JadeBeck@users.noreply.github.com>
2024-06-22 14:29:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1124 ms, Memory: 120028 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,70 @@
+import java.io.*;
+
+public class Main {
+    public static int[] arr, tmp;
+    public static long result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        tmp = new int[N + 1];
+        
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(br.readLine());
+        mergeSort(1, N);
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static void mergeSort(int start_idx, int end_idx) {
+        if (end_idx - start_idx < 1) return;
+
+        int middle_idx = start_idx + (end_idx - start_idx) / 2;
+        mergeSort(start_idx, middle_idx);
+        mergeSort(middle_idx + 1, end_idx);
+        
+        for (int i = start_idx; i <= end_idx; i++) tmp[i] = arr[i];
+        
+        int k = start_idx;
+        int index1 = start_idx;
+        int index2 = middle_idx + 1;
+        
+        while (index1 <= middle_idx && index2 <= end_idx) {
+            if (tmp[index1] > tmp[index2]) {
+                arr[k] = tmp[index2];
+                k++;
+                index2++;
+            } else {
+                arr[k] = tmp[index1];
+                k++;
+                index1++;
+            }
+        }
+        
+        while (index1 <= middle_idx) {
+            arr[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+        
+        while (index2 <= end_idx) {
+            arr[k] = tmp[index2];
+            k++;
+            index2++;
+        }
+    }
+}
+/**
+ 5
+ 5
+ 4
+ 3
+ 2
+ 1
+ */
